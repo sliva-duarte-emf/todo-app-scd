cec62c79a1126080eab9a64ea2880283b281e718
Silva <duarte.silvacorreia@studentfr.ch>
2026-02-05 08:16:58 +0100

    push 05.02.2026

--- src/backend/db.js
+++ src/backend/db.js
@@ -1,27 +1,15 @@
-// Use the MariaDB Node.js Connector
-var mariadb = require('mariadb');
-
+const mariadb = require('mariadb');
+const fs = require('fs');
 require('dotenv').config();
 
-// 1.) Access the Node File System package
-//const fs = require("fs");
-
-// 2.) Retrieve the Certificate Authority chain file (wherever you placed it - notice it's just in the Node project root here)
-//const serverCert = [fs.readFileSync("skysql_chain.pem", "utf8")];
-
-// Create a connection pool
-var pool = 
-  mariadb.createPool({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME //,
-    // 3.) Add an "ssl" property to the connection pool configuration, using the serverCert const defined above
-    /*ssl: {
-      ca: serverCert
-    }*/
-  });
+const pool = mariadb.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT, 10) || 3306,
+  connectionLimit: 10
+});
 
 // Expose the Pool object within this module
 module.exports = Object.freeze({
--- src/frontend/app.js
+++ src/frontend/app.js
@@ -1,8 +1,8 @@
 //const apiEndpoint = "https://fa-todo-backend-baqmes.azurewebsites.net/api/tasks";
-const apiEndpoint = "https://baqtestappbackend.blackglacier-c7b0844e.northeurope.azurecontainerapps.io/api/tasks";
+const apiEndpoint = `https://contenaire210silvacorreia.ashywater-99911d60.westus2.azurecontainerapps.io/api/tasks`;
 
 $(document).ready(function () {
-  // Charger les tâches au démarrage
+  console.log("API Endpoint :", apiEndpoint); // juste pour vérifier
   loadTasks();
 
   // Ajouter une nouvelle tâche
